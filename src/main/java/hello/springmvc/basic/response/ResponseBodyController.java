ce07ef5f3ed2a7e2dd767496eab798bc315e5709
DavidEugen-Mac <platonicojju@gmail.com>
2023-03-24 20:54:11 +0900

    Feat: @RestController
    @Controller
    @ResponseBody 와 같은 효과
    
    뷰 템플릿을 사용하는 것이 아니라, HTTP 메시지 바디에 직접 데이터를 입력

--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -8,13 +8,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
-@Controller
-@ResponseBody
+//@Controller
+//@ResponseBody
+@RestController
 public class ResponseBodyController {
 
     @GetMapping("/response-body-string-v1")
